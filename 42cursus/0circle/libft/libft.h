21362961077f4cd9ae8c052651f26c23c4434f7d
SNyoeng <devgmonk@naver.com>
2020-10-12 08:23:07 +0900

    10/12

--- 42cursus/0circle/libft/libft.h
+++ 42cursus/0circle/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: seungnle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/06 22:07:36 by seungnle          #+#    #+#             */
-/*   Updated: 2020/10/06 22:07:39 by seungnle         ###   ########.fr       */
+/*   Updated: 2020/10/12 08:02:03 by seungnle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,12 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
--- 42cursus/0circle/libft/main.c
+++ 42cursus/0circle/libft/main.c
@@ -1,24 +1,12 @@
 #include "libft.h"
 #include <stdio.h>
+#include <ctype.h>
 
 int dp[1001];
 
 int main(void)
 {
 	int i;
-	i = ft_atoi("  -123");
+	i = isalpha('!');
 	printf("%d\n", i);
-	
-
-/*	while (i < 4)
-		printf("c = %d\n", c[i++]);
-	i = 0;
-	d = b;
-	while (i < (int)sizeof(b))
-		printf("d1 = %d\n", d[i++]);
-	i = 0;
-	d = memcpy(a, b, 3);
-	while (i < (int)sizeof(b))
-		printf("d = %d\n", d[i++]);
-	printf("%d\n", i);*/
 }
