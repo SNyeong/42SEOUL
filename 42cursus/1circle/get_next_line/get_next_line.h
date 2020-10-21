fae82e9c4af9b34614127024b6ce4d5f6ba76219
Seungnyeong Lee <seungnle@c4r6s2.42seoul.kr>
2020-10-21 21:24:28 +0900

    10/21-1circle

---
+++ 42cursus/1circle/get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seungnle <seungnle@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/21 19:32:43 by seungnle          #+#    #+#             */
+/*   Updated: 2020/10/21 20:29:55 by seungnle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <limits.h>
+
+#endif
