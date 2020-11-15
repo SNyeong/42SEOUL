8f2fab1e02d7713f9f1e11cfdd0851bb47702eed
SNyoeng <devgmonk@naver.com>
2020-11-15 23:38:45 +0900

    11/15 home

--- 42cursus/1circle/ft_printf/libft/ft_strlcat.c
+++ 42cursus/1circle/ft_printf/libft/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seungnle <seungnle@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: seungnle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 01:54:35 by seungnle          #+#    #+#             */
-/*   Updated: 2020/10/24 17:18:17 by seungnle         ###   ########.fr       */
+/*   Updated: 2020/11/15 22:52:06 by seungnle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dstlen;
 	size_t	srclen;
 	size_t	i;
+	
 	dstlen = ft_strlen(dst);
 	srclen = ft_strlen(src);
 	if (dstsize < dstlen + 1)
