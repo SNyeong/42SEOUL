d9a6e92d4decd34c50a75861455ceec5248296c4
Seungnyeong Lee <seungnle@c1r7s1.42seoul.kr>
2020-11-16 19:15:24 +0900

    11/15 cluster

--- 42cursus/1circle/ft_printf/ft_printf.c
+++ 42cursus/1circle/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: seungnle <seungnle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/24 20:27:23 by seungnle          #+#    #+#             */
-/*   Updated: 2020/11/16 09:25:32 by seungnle         ###   ########.fr       */
+/*   Updated: 2020/11/16 19:12:11 by seungnle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -187,10 +187,11 @@ int			main(void)
 	char	*str;
 	int		a;
 
-	a = 0;
+	a = 10;
 	c = '0';
-	str = "123";
-	a = ft_printf("%5c\n%s\n%c\n%s\n%c\n", c, str, c, str, c);
-	printf("%d\n", a);
+	str = "321";
+	//a = ft_printf("%5c\n%s\n%c\n%s\n%c\n", c, str, c, str, c);
+	printf("%-0d\n", a);
+	printf("%d\n", (int)__LONG_LONG_MAX__);
 	return (0);
 }
