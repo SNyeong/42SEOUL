ed17cc316fd7f8e8ae4d9308d7822bd17ed10185
SNyoeng <devgmonk@naver.com>
2020-11-19 07:18:46 +0900

    11/19 home

--- 42cursus/1circle/ft_printf/ft_printf.c
+++ 42cursus/1circle/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: seungnle <seungnle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/24 20:27:23 by seungnle          #+#    #+#             */
-/*   Updated: 2020/11/17 21:37:52 by seungnle         ###   ########.fr       */
+/*   Updated: 2020/11/19 07:12:42 by seungnle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int			check_type(char **str, va_list ap, t_option option)
 		return (ft_char_count((char)va_arg(ap, int)));
 	else if (**str == 's')
 		return (ft_str_count(va_arg(ap, char *)));
-	a = option.o_len;
+	a = option.len;
 	return (0);
 }
 
@@ -33,22 +33,22 @@ int			check_len(char **str, va_list ap, t_option option)
 		++(*str);
 		if (**str == 'h')
 		{
-			option.o_len = 1;
+			option.len = 1;
 			++(*str);
 		}
 		else
-			option.o_len = 2;
+			option.len = 2;
 	}
 	else if (**str == 'l')
 	{
 		++(*str);
 		if (**str == 'l')
 		{
-			option.o_len = 3;
+			option.len = 3;
 			++(*str);
 		}
 		else
-			option.o_len = 4;
+			option.len = 4;
 	}
 	return (check_type(str, ap, option));
 }
@@ -62,13 +62,13 @@ int			check_precision(char **str, va_list ap, t_option option)
 		{
 			if (ft_isdigit(**str))
 			{
-				option.o_precision = 0;
+				option.precision = 0;
 				while (ft_isdigit(**str))
-					option.o_precision = option.o_precision * 10
+					option.precision = option.precision * 10
 					+ (*(*str++) - '0');
 			}
 			if (**str++ == '*')
-				option.o_precision = va_arg(ap, int);
+				option.precision = va_arg(ap, int);
 		}
 	}
 	return (check_len(str, ap, option));
@@ -80,12 +80,12 @@ int			check_width(char **str, va_list ap, t_option option)
 	{
 		if (ft_isdigit(**str))
 		{
-			option.o_width = 0;
+			option.width = 0;
 			while (ft_isdigit(**str))
-				option.o_width = option.o_width * 10 + (*(*str++) - '0');
+				option.width = option.width * 10 + (*(*str++) - '0');
 		}
 		if (**str++ == '*')
-			option.o_width = va_arg(ap, int);
+			option.width = va_arg(ap, int);
 	}
 	return (check_precision(str, ap, option));
 }
@@ -99,15 +99,15 @@ int			check_flags(char **str, va_list ap)
 	|| **str == '-' || **str == '0')
 	{
 		if (**str == '#')
-			option.o_flags[0] = 1;
+			option.flags[0] = 1;
 		else if (**str == '+')
-			option.o_flags[1] = 1;
+			option.flags[1] = 1;
 		else if (**str == ' ')
-			option.o_flags[2] = 1;
+			option.flags[2] = 1;
 		else if (**str == '-')
-			option.o_flags[3] = 1;
+			option.flags[3] = 1;
 		else if (**str == '0')
-			option.o_flags[4] = 1;
+			option.flags[4] = 1;
 		++(*str);
 	}
 	return (check_width(str, ap, option));
@@ -133,12 +133,12 @@ t_option	init_option(void)
 	a = -1;
 	while (++a < 5)
 	{
-		option.o_flags[a] = 0;
+		option.flags[a] = 0;
 	}
-	option.o_precision = -1;
-	option.o_width = -1;
-	option.o_len = 0;
-	option.o_type = 0;
+	option.precision = -1;
+	option.width = -1;
+	option.len = 0;
+	option.type = 0;
 	return (option);
 }
 
@@ -181,16 +181,14 @@ int			ft_printf(const char *str, ...)
 	return (count);
 }
 
-int			main(int argc, char **argv)
+int			test2(char **str)
 {
-	char	c;
-	int		*str;
-	int		a;
-	char	*buffer;
+	str[0]++;
+	return (2);
+}
 
-	buffer = ft_substr("1234567890", 0, 50);
-	printf("%s\n", buffer);
-	while (1)
-		;
-	return (0);
+int			test1(char **str)
+{
+	++*str;
+	return (test2(str));
 }
--- 42cursus/1circle/ft_printf/ft_printf.h
+++ 42cursus/1circle/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: seungnle <seungnle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 20:48:54 by seungnle          #+#    #+#             */
-/*   Updated: 2020/11/16 11:06:48 by seungnle         ###   ########.fr       */
+/*   Updated: 2020/11/19 05:03:11 by seungnle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,15 @@
 
 typedef struct	s_option
 {
-	int		o_flags[5];
-	int		o_width;
-	int		o_precision;
-	int		o_len;
-	char	o_type;
+	int		flags[5];
+	int		width;
+	int		precision;
+	int		len;
+	char	type;
+
+	int		n_idx;
+	int		*n_value;
+	void	**n_addr;
 }				t_option;
 
 int				ft_printf(const char *str, ...);
